0e16c403b0e4f38d6c04395d63d67477bcb595bd
Gary A. Stafford <garystafford@users.noreply.github.com>
2024-04-16 12:57:05 -0400

    Update app.py adding Opus

--- app.py
+++ app.py
@@ -365,6 +365,7 @@ Important: if no ads were provided, do not produce the analysis."""
             options=[
                 "anthropic.claude-3-sonnet-20240229-v1:0",
                 "anthropic.claude-3-haiku-20240307-v1:0",
+                "anthropic.claude-3-opus-20240229-v1:0",
             ],
         )
 
