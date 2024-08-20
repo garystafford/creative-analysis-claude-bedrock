99f7df3f96a66731f3c58eb62ba1451190100c53
Gary A. Stafford <garystafford@users.noreply.github.com>
2024-08-20 09:30:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -391,7 +391,7 @@ Important: if no ads were provided, do not produce the analysis."""
                 "anthropic.claude-3-5-sonnet-20240620-v1:0",
                 "anthropic.claude-3-haiku-20240307-v1:0",
                 "anthropic.claude-3-sonnet-20240229-v1:0",
-                "anthropic.claude-3-haiku-20240307-v1:0",
+                "anthropic.claude-3-opus-20240229-v1:0",
             ],
         )
 
