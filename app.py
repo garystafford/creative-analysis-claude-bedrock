dc9416eea04cb682a921c8c1da48d364374c65d2
Gary A. Stafford <garystafford@users.noreply.github.com>
2024-08-21 16:16:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import base64
 import datetime
 import json
 import logging
-import os
 from io import StringIO
 from tempfile import NamedTemporaryFile
 
@@ -21,7 +20,9 @@ from botocore.exceptions import ClientError
 from PIL import Image
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 
 def run_multi_modal_prompt(
@@ -115,13 +116,12 @@ def build_request(user_prompt, file_paths):
             st.session_state.top_k,
         )
 
-        print(json.dumps(response, indent=4))
+        logger.info(json.dumps(response, indent=4))
 
         return response
     except ClientError as err:
         message = err.response["Error"]["Message"]
-        logger.error("A client error occurred: %s", message)
-        print("A client error occurred: " + format(message))
+        logger.error(f"A client error occurred: {message}")
         return None
 
 
@@ -336,7 +336,7 @@ Important: if no ads were provided, do not produce the analysis."""
                         }
                     )
 
-            print(f"Prompt: {user_prompt}")
+            logger.info(f"Prompt: {user_prompt}")
 
         submitted = st.form_submit_button("Submit")
         if submitted:
@@ -355,7 +355,7 @@ Important: if no ads were provided, do not produce the analysis."""
                     for file_path in file_paths:
                         st.image(file_path["file_path"], caption="", width=400)
             except UnboundLocalError as e:
-                print("No files uploaded")
+                logger.error("No files uploaded")
 
             with st.spinner(text="Analyzing..."):
                 current_time1 = datetime.datetime.now()
@@ -400,11 +400,11 @@ Important: if no ads were provided, do not produce the analysis."""
         )
 
         st.session_state.temperature = st.slider(
-            "temperature", min_value=0.0, max_value=1.0, value=0.5, step=0.01
+            "temperature", min_value=0.0, max_value=1.0, value=0.2, step=0.05
         )
 
         st.session_state.top_p = st.slider(
-            "top_p", min_value=0.0, max_value=1.0, value=0.999, step=0.001
+            "top_p", min_value=0.0, max_value=1.0, value=0.999, step=0.01
         )
 
         st.session_state.top_k = st.slider(
